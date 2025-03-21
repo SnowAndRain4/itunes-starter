f9bffac6837e2ce1657d3a90785f990ff84b4778
SnowAndRain4 <marchidalgo397@gmail.com>
2025-03-21 16:22:09 +0000

    done and done

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-function run() {
+//these variables shall act as the STOCKS
+let TSLA = "Value 1"; //will use getPositions command for all of these shares normally
+let BLACKROCK = "Value 2";
+let AMD = "Value 3";
+let NVDYA = "Value 4";
+let APPLE = "Value 5";
+let SandP = "Value 6";
+let NASDAQ = "Value 7";
+let AMAZON = "Value 8";
 
-    var artist = "drake";
 
-    $.ajax({
-        url: 'https://itunes.apple.com/search?media=music&term=' + artist,
-        dataType: "json",
-        success: process
-    });
+let StockArray = [var1, var2, var3, var4, var5, var6, var7, var8];
+
+//will give me my values for all of my stocks, I will use this in order to
+function gatherStockValues(array) {
+    for (let i = 0; i < array.length; i++) {
+        //array[i] = getPosition()
+          return(array[i])
+    }
+}
+//sorts the array of stocks from lowest to highest
+function chooseLowestStock(array){
+    for(let i = 0; i < 8; i++){
+        if(array[i] > [i++]){
+            let storage = array[i]
+            array[i++] = array[i]
+            storage = array[i++]
+        }
+    }
 }
 
-function process(data) {
-    
-    console.log(data)
-    
+function ChooseStockToBuy{
+
 }
